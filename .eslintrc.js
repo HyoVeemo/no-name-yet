4193ef069f63adaadd0ce20a1cb75ec20d324aaa
HyoseonJegal <dott.goo@gmail.com>
2021-03-23 17:12:03 +0900

    chore: eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,29 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
+    '@typescript-eslint/no-floating-promises': 'error',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-cycle': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false
+      }
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'if' },
+      { blankLine: 'always', prev: '*', next: 'for' },
+      { blankLine: 'always', prev: '*', next: 'try' },
+      { blankLine: 'always', prev: '*', next: 'while' },
+      { blankLine: 'always', prev: '*', next: 'break' }
+    ]
+  }
 };
+
+
